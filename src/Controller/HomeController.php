c637e446ae12d033e57c63f98253cdb766d8a412
Hamada Tina <falatina.ahamada@gmail.com>
2021-05-29 21:07:30 +0300

    auth fonctionel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,18 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
+    #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
-    
-    /**
-     * @Route("/mentions-legales", name="mentions")
-     */
-    public function mention(){
 
-    }
 }
